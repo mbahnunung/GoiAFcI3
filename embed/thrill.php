f2133957ad06952f64b58b16c3c17123e790936e
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-12 11:16:56 +0700

    Add files via upload

---
+++ embed/thrill.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+       <!-- Start Meta Ads -->
+       <meta name="platform" content="desktop" />
+       <meta name="site_name" content="streaming" />
+       <!-- End Meta Ads -->
+       <title>Live Streaming</title>
+       <style>
+           body {
+               margin:0;
+               padding:0;
+               overflow: hidden;
+           }
+       </style>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script src="https://warningfm.github.io/ajax/libs/hola_player/package/dist/hola_player.js"></script>
+	   
+       <div align="center">
+        <video poster="https://2.bp.blogspot.com/-qYdp0fptI7E/W9zRuz2e7_I/AAAAAAAAJ78/E4143bz-rHIJJqPZFGuVJK-NZGC4NJHeACLcBGAs/s1600/Thrill_logo.jpg" id="video-player" class="video-js vjs-fluid vjs-default-skin" height="340" width="596" controls autoplay>
+        <source src="https://kastproxy-us.herokuapp.com/http://rr2.dens.tv/s/s34/index.m3u8" type="application/x-mpegURL" />
+       </video>
+     <script>
+      window.hola_player();
+     </script>
+     </div>
+   </body>
+</html>
---
+++ embed/tv9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+       <!-- Start Meta Ads -->
+       <meta name="platform" content="desktop" />
+       <meta name="site_name" content="streaming" />
+       <!-- End Meta Ads -->
+       <title>Live Streaming</title>
+       <style>
+           body {
+               margin:0;
+               padding:0;
+               overflow: hidden;
+           }
+       </style>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script src="https://warningfm.github.io/ajax/libs/hola_player/package/dist/hola_player.js"></script>
+	   
+       <div align="center">
+        <video poster="https://4.bp.blogspot.com/-DifMWPn0CBM/W92WwQgP3TI/AAAAAAAAAsM/Z27Vhx7_8Z0-uv3ZxhRHILHgq8emrOYhwCLcBGAs/s1600/tv9nusantara.jpg?v=8.3.2" id="video-player" class="video-js vjs-fluid vjs-default-skin" height="340" width="596" controls autoplay>
+        <source src="https://zet.pluginsandthemes.ro/http://wowza60.indostreamserver.com:1935/tv9nusantara/live/playlist.m3u8" type="application/x-mpegURL" />
+       </video>
+     <script>
+      window.hola_player();
+     </script>
+     </div>
+   </body>
+</html>
