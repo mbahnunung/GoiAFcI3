c501d42257afbf54b588a4d1315edef21e38302d
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-12 12:41:49 +0700

    Add files via upload

---
+++ embed/tv1_rtm.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+       <!-- Start Meta Ads -->
+       <meta name="platform" content="desktop" />
+       <meta name="site_name" content="streaming" />
+       <!-- End Meta Ads -->
+       <title>Live Streaming</title>
+       <style>
+           body {
+               margin:0;
+               padding:0;
+               overflow: hidden;
+           }
+       </style>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script src="https://cdn1.mbahnunungonline.net/v3/hola_player/hola_player.js"></script>
+	   
+       <div align="center">
+        <video poster="../img/thumbs/Logo_of_TV1.png?v=8.3.2" id="video-player" class="video-js vjs-fluid vjs-default-skin" height="340" width="596" controls autoplay>
+        <source src="https://rtm1mobile.secureswiftcontent.com/Origin01/ngrp:RTM1/playlist.m3u8" type="application/x-mpegURL" />
+       </video>
+     <script>
+      window.hola_player();
+     </script>
+     </div>
+   </body>
+</html>
