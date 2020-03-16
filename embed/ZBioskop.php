63ae62566f400e6c7d6f3d3d103a0968d1f82e0f
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-17 03:01:58 +0700

    Add files via upload

---
+++ embed/ZBioskop.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+       <!-- Start Meta Ads -->
+       <meta name="platform" content="desktop" />
+       <meta name="site_name" content="streaming" />
+       <!-- End Meta Ads -->
+       <title>Live Streaming</title>
+       <style>
+           body {
+               margin:0;
+               padding:0;
+               overflow: hidden;
+           }
+       </style>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script src="https://cdn1.mbahnunungonline.net/v3/hola_player/hola_player.js"></script>
+	   
+       <div align="center">
+        <video poster="https://3.bp.blogspot.com/-IAJL70eGt68/XMY7x2zCE4I/AAAAAAAALXU/SkAnsL54b7wFR4R-8Bv5TbP_aueIMqRzACLcBGAs/s1600/zee-bioskop.jpg?v=8.3.2" id="video-player" class="video-js vjs-fluid vjs-default-skin" height="340" width="596" controls autoplay>
+        <source src="https://t4.mbahnunungonline.net/qwr9ew/s/s32/index.m3u8" type="application/x-mpegURL" />
+       </video>
+     <script>
+      window.hola_player();
+     </script>
+     </div>
+   </body>
+</html>
