99377dca49823cf14009a566bbf96d26c435d0ac
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-21 02:05:53 +0700

    Add files via upload

---
+++ embed/zoom.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="utf-8" />
+       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+       <!-- Start Meta Ads -->
+       <meta name="platform" content="desktop" />
+       <meta name="site_name" content="streaming" />
+       <!-- End Meta Ads -->
+       <title>Live Streaming</title>
+       <style>
+           body {
+               margin:0;
+               padding:0;
+               overflow: hidden;
+           }
+       </style>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+       <script src="https://cdn1.mbahnunungonline.net/v3/hola_player/hola_player.js"></script>
+	   
+       <div align="center">
+        <video poster="https://3.bp.blogspot.com/-rpqZbTYD3PA/W-GLXA9JmiI/AAAAAAAAJ_U/q4BiDXuSvrI1KM3oizrxIoQq75xXXoeiwCLcBGAs/s1600/zoom-tv-logo.jpg?v=8.3.2" id="video-player" class="video-js vjs-fluid vjs-default-skin" height="340" width="596" controls autoplay>
+        <source src="https://zoomweblive-lh.akamaihd.net/i/Zoomweb_1@348071/master.m3u8" type="application/x-mpegURL" />
+       </video>
+     <script>
+      window.hola_player();
+     </script>
+     </div>
+   </body>
+</html>
